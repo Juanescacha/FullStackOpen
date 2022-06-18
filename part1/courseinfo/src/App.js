e251aa85191e9c12f7145c9a213691c13d7a370a
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-06-18 16:58:12 -0500

    Exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,26 +2,28 @@ const Header = (props) => {
   return <h1> {props.course} </h1>;
 };
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part2} exercises={props.exercises3} />
+    </div>
   );
 };
 
 const Total = (props) => {
   return (
     <p>
-      Number of exercises
+      Number of exercises{" "}
       {props.exercises1 + props.exercises2 + props.exercises3}
     </p>
   );
