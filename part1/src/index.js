1a46f5894313f283cf8ac5c1cc701dc877a8ecc5
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-06-18 16:08:59 -0500

    Introduction to React

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import ReactDOM from "react-dom/client";
 import "./index.css";
 
-/* comment the base code and use the one teach on the course
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-*/
-
-const App = () => (
-  <div>
-    <p>Hellow world</p>
-  </div>
-);
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
