0744e43b65e8edf0b159352dcfec26601010c1e7
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-10 00:07:07 -0500

    Exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,18 @@ const Button = ({ text, handleClick }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      all {good + neutral + bad}
+      <br />
+      average {(good * 1 + bad * -1) / (good + neutral + bad)}
+      <br />
+      positive {(good / (good + neutral + bad)) * 100}%
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -35,11 +47,7 @@ const App = () => {
       <br />
       bad {bad}
       <br />
-      all {good + neutral + bad}
-      <br />
-      average {(good * 1 + bad * -1) / (good + neutral + bad)}
-      <br />
-      positive {(good / (good + neutral + bad)) * 100}%
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
