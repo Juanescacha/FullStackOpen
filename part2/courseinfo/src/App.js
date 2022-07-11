e140c25f019daa1904df5dc131c49ad8f244d49f
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-11 12:12:51 -0500

    We start the code with the model answers

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </>
+);
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total
+        sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
