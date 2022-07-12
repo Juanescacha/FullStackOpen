bb6c74e4dc504442f79d26329c6c7bbb65817cb8
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-12 13:48:10 -0500

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ newSearchName, handleSearchName }) => (
   <>
@@ -38,15 +39,22 @@ const Persons = ({ personsToShow }) => (
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearchName, setNewSearchName] = useState("");
+
+  const fetchHook = () => {
+    console.log("effecto");
+
+    const eventHandler = (response) => {
+      console.log("promesa completada");
+      setPersons(response.data);
+    };
+    axios.get("http://localhost:3001/persons").then(eventHandler);
+  };
+
+  useEffect(fetchHook, []);
 
   const addPerson = (event) => {
     event.preventDefault();
