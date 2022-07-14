4543f515753b69fb8556f9a29df5db5730918a29
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-14 11:55:35 -0500

    Exercise 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,14 +1,47 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 
-const Countries = ({ countriesToShow }) => (
-  <>
-    {countriesToShow.map((country, i) => (
-      <div key={i}>{country.name.common}</div>
-    ))}
-  </>
-)
-
+const Countries = ({ countriesToShow, newSearchCountry }) => {
+  if (newSearchCountry === "") {
+    return <div></div>
+  } else {
+    if (countriesToShow.length > 10) {
+      return <div>Too many matches, specify another filter</div>
+    } else {
+      if (countriesToShow.length > 1) {
+        return (
+          <>
+            {countriesToShow.map((country, i) => (
+              <div key={i}>{country.name.common}</div>
+            ))}
+          </>
+        )
+      } else {
+        if (countriesToShow.length == 1) {
+          return (
+            <div>
+              <h1>{countriesToShow[0].name.common}</h1>
+              <div>capital {countriesToShow[0].capital}</div>
+              <div>area {countriesToShow[0].area}</div>
+              <h3>languages:</h3>
+              <ul>
+                {Object.values(countriesToShow[0].languages).map(value => (
+                  <li key={value}>{value}</li>
+                ))}
+              </ul>
+              <img
+                alt={countriesToShow[0].flag}
+                src={countriesToShow[0].flags.png}
+              />
+            </div>
+          )
+        } else {
+          return <div>0 matches, try another search</div>
+        }
+      }
+    }
+  }
+}
 function App() {
   const [newSearchCountry, setNewSearchCountry] = useState("")
   const [countries, setCountries] = useState([])
@@ -25,20 +58,20 @@ function App() {
 
   useEffect(fetchHook, [])
 
-  const countriesToShow =
-    !newSearchCountry || newSearchCountry === ""
-      ? countries
-      : countries.filter(country =>
-          country.name.common
-            .toLowerCase()
-            .includes(newSearchCountry.toLocaleLowerCase())
-        )
+  const countriesToShow = countries.filter(country =>
+    country.name.common
+      .toLowerCase()
+      .includes(newSearchCountry.toLocaleLowerCase())
+  )
 
   return (
     <div>
-      find countries
+      find countries{" "}
       <input value={newSearchCountry} onChange={handleSearchCountry} />
-      <Countries countriesToShow={countriesToShow} />
+      <Countries
+        countriesToShow={countriesToShow}
+        newSearchCountry={newSearchCountry}
+      />
     </div>
   )
 }
