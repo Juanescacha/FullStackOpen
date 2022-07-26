9b8e3d328ad774af2690ecf3d46146db15145a80
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-26 18:32:20 -0500

    Exercise 3.15

--- part3/index.js
+++ part3/index.js
@@ -41,9 +41,10 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-  Person.deleteOne({ id: request.params.id }).then(res => {
+  Person.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
   })
+  //.catch(error => next(error))
 })
 
 app.post("/api/persons", (request, response) => {
