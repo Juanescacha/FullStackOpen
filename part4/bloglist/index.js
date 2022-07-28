8b3d2a4d7b9637eb12a76a3a90c8e9126b70aab2
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-28 13:51:55 -0500

    refactoring utils: /config and /logger

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,9 +1,10 @@
-require("dotenv").config()
 const http = require("http")
 const express = require("express")
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -14,8 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -34,7 +34,6 @@ app.post("/api/blogs", (request, response) => {
   })
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
