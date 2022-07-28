223aebd624bc660bc72ce010fcd9fb3c36a0bcd5
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-28 13:39:42 -0500

    App working single file

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const http = require("http")
 const express = require("express")
 const app = express()
@@ -13,7 +14,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema)
 
-const mongoUrl = "mongodb://localhost/bloglist"
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -33,7 +34,7 @@ app.post("/api/blogs", (request, response) => {
   })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
