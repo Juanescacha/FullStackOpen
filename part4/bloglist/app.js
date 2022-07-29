588383a534f89dedc93cd730db30f0eba0731507
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-28 19:43:50 -0500

    Exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,30 @@
+const config = require("./utils/config")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const blogsRouter = require("./controllers/blogs")
+const notesRouter = require("./controllers/blogs")
+const middleware = require("./utils/middleware")
+const logger = require("./utils/logger")
+const mongoose = require("mongoose")
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB")
+  })
+  .catch(error => {
+    logger.error("error connecting to MongoDB:", error.message)
+  })
+
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.unknowEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,39 +1,10 @@
+const app = require("./app")
 const http = require("http")
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
 const config = require("./utils/config")
 const logger = require("./utils/logger")
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model("Blog", blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
