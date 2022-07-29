2662342f52f686ced79c09d049df022f46334b94
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-29 12:52:09 -0500

    Exercise 4.4

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -1,8 +1,69 @@
 const listHelper = require("../utils/list_helper")
 
-test("dummy returns one", () => {
-  const blogs = []
-  const result = listHelper.dummy(blogs)
+describe("dummy", () => {
+  test("returns one", () => {
+    const blogs = []
+    const result = listHelper.dummy(blogs)
 
-  expect(result).toBe(1)
+    expect(result).toBe(1)
+  })
+})
+
+describe("total likes", () => {
+  const listEmpty = []
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ]
+  const blogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 4,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 3,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 2,
+      __v: 0,
+    },
+  ]
+  test("of empty list is zero", () => {
+    expect(listHelper.totalLikes(listEmpty)).toBe(0)
+  })
+
+  test("when list has only one blog equals the likes of that", () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+  })
+
+  test("of a bigger list is calculated right", () => {
+    expect(listHelper.totalLikes(blogs)).toBe(14)
+  })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = blogs => {
-  return 1
+  if (blogs) {
+    return 1
+  }
+}
+
+const totalLikes = blogs => {
+  const sumLikes = (sum, blog) => sum + blog.likes
+  return blogs.reduce(sumLikes, 0)
 }
 
 module.exports = {
   dummy,
+  totalLikes,
 }
