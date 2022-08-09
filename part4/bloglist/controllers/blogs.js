56f7204e3fddeada00cee71e2e892438cc282931
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-08-08 21:19:39 -0500

    Exercise 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getTokenFrom = request => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
@@ -10,11 +19,18 @@ blogsRouter.get("/", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body
 
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(404).json({ error: "token missing or invalid" })
+  }
+
   if (!body.title || !body.url) {
     return response.status(400).json({ error: "Title or URL Missing" })
   }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -13,13 +13,16 @@ const unknowEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" })
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "invalid token" })
   }
+
+  logger.error(error.message)
+
   next(error)
 }
 
