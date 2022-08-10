0b19e148b8f680173a7cce600aa0719c9415216a
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-08-10 16:54:03 -0500

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? "" : "none" }
+  const hideWhenVisible = { display: visible ? "none" : "" }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <>
+      <div style={blogStyle}>
+        <div style={hideWhenVisible}>
+          {blog.title} {blog.author}
+          <button onClick={toggleVisibility}>view</button>
+        </div>
+        <div style={showWhenVisible}>
+          {blog.title} {blog.author}
+          <button onClick={toggleVisibility}>hide</button>
+          <br />
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>like</button>
+          <br />
+          {blog.author}
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default Blog
