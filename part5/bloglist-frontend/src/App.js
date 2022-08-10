85de053ded05058e0135cf2bd01e8abebf28aabf
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-08-10 16:10:18 -0500

    Exercise 5.5

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,4 +1,10 @@
-import { useState, useEffect } from "react"
+import {
+  useState,
+  useEffect,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
 import loginService from "./services/login"
@@ -13,7 +19,39 @@ const Notification = ({ message }) => {
   return <div className={type}>{message[0]}</div>
 }
 
+const Toggable = forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? "none" : "" }
+  const showWhenVisible = { display: visible ? "" : "none" }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility,
+    }
+  })
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <br />
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  )
+})
+
 const App = () => {
+  const blogFormRef = useRef()
+
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState("")
@@ -63,6 +101,7 @@ const App = () => {
 
     try {
       await blogService.create(blog)
+      blogFormRef.current.toggleVisibility()
       setAuthor("")
       setTitle("")
       setUrl("")
@@ -95,36 +134,39 @@ const App = () => {
   if (user === null) {
     return (
       <div>
-        <h2>Log in to application</h2>
+        <h1>Blogs</h1>
         <Notification message={message} />
-        <form onSubmit={handleLogin}>
-          <div>
-            username{" "}
-            <input
-              type="text"
-              name="Username"
-              value={username}
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password{" "}
-            <input
-              type="password"
-              name="Password"
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
+        <Toggable buttonLabel="login">
+          <h2>Log in</h2>
+          <form onSubmit={handleLogin}>
+            <div>
+              username{" "}
+              <input
+                type="text"
+                name="Username"
+                value={username}
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div>
+              password{" "}
+              <input
+                type="password"
+                name="Password"
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
+        </Toggable>
       </div>
     )
   }
 
   return (
     <div>
-      <h2>blogs</h2>
+      <h1>Blogs</h1>
       <Notification message={message} />
       <div>
         {user.name} logged in{" "}
@@ -132,36 +174,36 @@ const App = () => {
           logout
         </button>
       </div>
-      <br />
-      {/* Exercise 5.3 */}
-      <h2>create new</h2>
-      <form onSubmit={handleCreate}>
-        title:
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
-        <br />
-        author:
-        <input
-          type="text"
-          name="author"
-          value={author}
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-        <br />
-        url:
-        <input
-          type="URL"
-          name="url"
-          value={url}
-          onChange={({ target }) => setUrl(target.value)}
-        />
-        <br />
-        <button type="submit">create</button>
-      </form>
+      <Toggable buttonLabel="new blog" ref={blogFormRef}>
+        <h1>create new</h1>
+        <form onSubmit={handleCreate}>
+          title:{" "}
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+          <br />
+          author:{" "}
+          <input
+            type="text"
+            name="author"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+          <br />
+          url:{" "}
+          <input
+            type="URL"
+            name="url"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+          <br />
+          <button type="submit">create</button>
+        </form>
+      </Toggable>
       <br />
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
