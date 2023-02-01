cefec1845e05dbd729777182015829872397231e
Juanescacha <juanescacha@hotmail.com>
2023-02-01 16:28:40 -0500

    4 tabs identation

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -4,37 +4,37 @@ const baseUrl = "/api/blogs"
 let token = null
 
 const setToken = newToken => {
-  token = `bearer ${newToken}`
+	token = `bearer ${newToken}`
 }
 
 const getToken = () => {
-  return token
+	return token
 }
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+	const request = axios.get(baseUrl)
+	return request.then(response => response.data)
 }
 
 const create = async newObject => {
-  const config = { headers: { Authorization: token } }
+	const config = { headers: { Authorization: token } }
 
-  const response = await axios.post(baseUrl, newObject, config)
-  return response.data
+	const response = await axios.post(baseUrl, newObject, config)
+	return response.data
 }
 
 const addLike = async (newObject, id) => {
-  const config = { headers: { Authorization: token } }
+	const config = { headers: { Authorization: token } }
 
-  const response = await axios.put(`${baseUrl}/${id}`, newObject, config)
-  return response.data
+	const response = await axios.put(`${baseUrl}/${id}`, newObject, config)
+	return response.data
 }
 
 const remove = async id => {
-  const config = { headers: { Authorization: token } }
+	const config = { headers: { Authorization: token } }
 
-  const response = await axios.delete(`${baseUrl}/${id}`, config)
-  return response.data
+	const response = await axios.delete(`${baseUrl}/${id}`, config)
+	return response.data
 }
 
 const blogService = { setToken, getAll, create, getToken, addLike, remove }
--- part5/bloglist-frontend/src/services/login.js
+++ part5/bloglist-frontend/src/services/login.js
@@ -2,8 +2,8 @@ const axios = require("axios")
 const baseUrl = "/api/login"
 
 const login = async credentials => {
-  const response = await axios.post(baseUrl, credentials)
-  return response.data
+	const response = await axios.post(baseUrl, credentials)
+	return response.data
 }
 
 const loginService = { login }
