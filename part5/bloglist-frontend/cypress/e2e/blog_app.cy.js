1cce5f56c0e63a6d97c3262bbbf6bc8902e71d43
Juanescacha <juanescacha@hotmail.com>
2023-02-06 10:57:34 -0500

    setting up cypress

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,10 @@
+describe("blog app", function () {
+	beforeEach(function () {
+		cy.request("POST", "http://localhost:3001/api/testing/reset")
+		cy.visit("http://localhost:3000")
+	})
+
+	it("login form is shown", function () {
+		cy.contains("login")
+	})
+})
