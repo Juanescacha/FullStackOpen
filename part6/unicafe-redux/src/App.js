b86f6e910781f39f049914c22e941487ebbb5830
Juanescacha <juanescacha@hotmail.com>
2023-02-06 23:34:46 -0500

    clone part1/unicafe

---
+++ part6/unicafe-redux/src/App.js
@@ -0,0 +1,73 @@
+import { useState } from "react"
+
+const Button = ({ text, handleClick }) => {
+	return <button onClick={handleClick}>{text}</button>
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+	if (good === 0 && neutral === 0 && bad === 0) {
+		return <h3>No feedback given</h3>
+	}
+
+	return (
+		<table>
+			<tbody>
+				<StatisticLine text="good" value={good} />
+				<StatisticLine text="neutral" value={neutral} />
+				<StatisticLine text="bad" value={bad} />
+
+				<StatisticLine text="all" value={good + neutral + bad} />
+				<StatisticLine
+					text="average"
+					value={(good * 1 + bad * -1) / (good + neutral + bad)}
+				/>
+				<StatisticLine
+					text="positive"
+					value={(good / (good + neutral + bad)) * 100 + "%"}
+				/>
+			</tbody>
+		</table>
+	)
+}
+
+const StatisticLine = ({ text, value }) => {
+	return (
+		<tr>
+			<td>{text}</td>
+			<td>{value}</td>
+		</tr>
+	)
+}
+
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+	const handleGood = () => {
+		setGood(good + 1)
+	}
+
+	const handleNeutral = () => {
+		setNeutral(neutral + 1)
+	}
+
+	const handleBad = () => {
+		setBad(bad + 1)
+	}
+
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<Button text="good" handleClick={handleGood} />
+			<Button text="neutral" handleClick={handleNeutral} />
+			<Button text="bad" handleClick={handleBad} />
+
+			<h1>statistics</h1>
+			<Statistics good={good} neutral={neutral} bad={bad} />
+		</div>
+	)
+}
+
+export default App
