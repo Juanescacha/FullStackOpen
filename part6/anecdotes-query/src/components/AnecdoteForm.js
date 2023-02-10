c11210100a7d544e7c17420cbe3bafee0a6864b2
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-02-10 07:48:09 -0500

    Exercise 6.21

--- part6/anecdotes-query/src/components/AnecdoteForm.js
+++ part6/anecdotes-query/src/components/AnecdoteForm.js
@@ -1,9 +1,20 @@
+import { useMutation, useQueryClient } from "react-query"
+import { createAnecdote } from "../requests"
+
 const AnecdoteForm = () => {
+	const queryClient = useQueryClient()
+
+	const newAnecdoteMutation = useMutation(createAnecdote, {
+		onSuccess: () => {
+			queryClient.invalidateQueries("anecdotes")
+		},
+	})
+
 	const onCreate = event => {
 		event.preventDefault()
 		const content = event.target.anecdote.value
 		event.target.anecdote.value = ""
-		console.log(`new anecdote: ${content}`)
+		newAnecdoteMutation.mutate({ content, votes: 0 })
 	}
 
 	return (
--- part6/anecdotes-query/src/requests.js
+++ part6/anecdotes-query/src/requests.js
@@ -4,3 +4,6 @@ const baseUrl = "http://localhost:3001"
 
 export const getAnecdotes = () =>
 	axios.get(`${baseUrl}/anecdotes`).then(res => res.data)
+
+export const createAnecdote = content =>
+	axios.post(`${baseUrl}/anecdotes`, content).then(res => res.data)
