2e0b87322d42c25e64e3bd5c93a72cd7c51869e8
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-02-10 00:19:55 -0500

    identation change

--- part6/anecdotes-query/src/components/AnecdoteForm.js
+++ part6/anecdotes-query/src/components/AnecdoteForm.js
@@ -1,21 +1,20 @@
 const AnecdoteForm = () => {
+	const onCreate = event => {
+		event.preventDefault()
+		const content = event.target.anecdote.value
+		event.target.anecdote.value = ""
+		console.log("new anecdote")
+	}
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
-
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name="anecdote" />
+				<button type="submit">create</button>
+			</form>
+		</div>
+	)
 }
 
 export default AnecdoteForm
--- part6/anecdotes-query/src/components/Notification.js
+++ part6/anecdotes-query/src/components/Notification.js
@@ -1,18 +1,14 @@
 const Notification = () => {
-  const style = {
-    border: 'solid',
-    padding: 10,
-    borderWidth: 1,
-    marginBottom: 5
-  }
-  
-  if (true) return null
+	const style = {
+		border: "solid",
+		padding: 10,
+		borderWidth: 1,
+		marginBottom: 5,
+	}
 
-  return (
-    <div style={style}>
-      
-    </div>
-  )
+	if (true) return null
+
+	return <div style={style}></div>
 }
 
 export default Notification
