39ff075a08126435757efc591aa21e18a90a2a9f
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-02-11 21:03:06 -0500

    Exercise 7.4

--- part7/anecdotes-routed/src/App.js
+++ part7/anecdotes-routed/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Routes, Route, Link, useMatch, useNavigate } from "react-router-dom"
+import { useField } from "./hooks"
 
 const Menu = () => {
 	const padding = {
@@ -70,17 +71,17 @@ const Footer = () => (
 )
 
 const CreateNew = ({ addNew }) => {
-	const [content, setContent] = useState("")
-	const [author, setAuthor] = useState("")
-	const [info, setInfo] = useState("")
+	const content = useField("text")
+	const author = useField("text")
+	const info = useField("text")
 	const navigate = useNavigate()
 
 	const handleSubmit = e => {
 		e.preventDefault()
 		addNew({
-			content,
-			author,
-			info,
+			content: content.value,
+			author: author.value,
+			info: info.value,
 			votes: 0,
 		})
 		navigate("/")
@@ -92,27 +93,15 @@ const CreateNew = ({ addNew }) => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input
-						name="content"
-						value={content}
-						onChange={e => setContent(e.target.value)}
-					/>
+					<input name="content" {...content} />
 				</div>
 				<div>
 					author
-					<input
-						name="author"
-						value={author}
-						onChange={e => setAuthor(e.target.value)}
-					/>
+					<input name="author" {...author} />
 				</div>
 				<div>
 					url for more info
-					<input
-						name="info"
-						value={info}
-						onChange={e => setInfo(e.target.value)}
-					/>
+					<input name="info" {...info} />
 				</div>
 				<button>create</button>
 			</form>
