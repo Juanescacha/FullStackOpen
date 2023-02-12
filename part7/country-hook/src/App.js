b0a2b805f329c2537bc329c7c4b19ff4431e57e0
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-02-12 12:28:41 -0500

    indentation

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,71 +1,71 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
+const useField = type => {
+	const [value, setValue] = useState("")
 
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
+	const onChange = event => {
+		setValue(event.target.value)
+	}
 
-  return {
-    type,
-    value,
-    onChange
-  }
+	return {
+		type,
+		value,
+		onChange,
+	}
 }
 
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+const useCountry = name => {
+	const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+	useEffect(() => {})
 
-  return country
+	return country
 }
 
 const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
+	if (!country) {
+		return null
+	}
 
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
+	if (!country.found) {
+		return <div>not found...</div>
+	}
 
-  return (
-    <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
-    </div>
-  )
+	return (
+		<div>
+			<h3>{country.data.name} </h3>
+			<div>capital {country.data.capital} </div>
+			<div>population {country.data.population}</div>
+			<img
+				src={country.data.flag}
+				height="100"
+				alt={`flag of ${country.data.name}`}
+			/>
+		</div>
+	)
 }
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+	const nameInput = useField("text")
+	const [name, setName] = useState("")
+	const country = useCountry(name)
 
-  const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
+	const fetch = e => {
+		e.preventDefault()
+		setName(nameInput.value)
+	}
 
-  return (
-    <div>
-      <form onSubmit={fetch}>
-        <input {...nameInput} />
-        <button>find</button>
-      </form>
+	return (
+		<div>
+			<form onSubmit={fetch}>
+				<input {...nameInput} />
+				<button>find</button>
+			</form>
 
-      <Country country={country} />
-    </div>
-  )
+			<Country country={country} />
+		</div>
+	)
 }
 
 export default App
