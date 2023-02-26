d2a628778d3575aa2a91156dc6eae711832fabbd
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-02-25 20:39:52 -0500

    Exercise 8.7

--- part8/library/index.js
+++ part8/library/index.js
@@ -124,6 +124,11 @@ type Mutation {
 		published: Int!
 		genres: [String!]!
 		): Book
+
+	editAuthor(
+		name: String!
+		setBornTo: Int!
+	): Author
 }
 `
 
@@ -168,6 +173,17 @@ const resolvers = {
 				authors = authors.concat(author)
 			}
 			return book
+		},
+		editAuthor: (root, args) => {
+			const author = authors.find(a => a.name === args.name)
+			if (!author) {
+				return null
+			}
+			const updatedAuthor = { ...author, born: args.setBornTo }
+			authors = authors.map(a =>
+				a.name === args.name ? updatedAuthor : a
+			)
+			return updatedAuthor
 		},
 	},
 }
