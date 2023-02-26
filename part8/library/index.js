a2971c9830dbd10490824bf837985270081d3888
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-02-25 19:42:48 -0500

    Exercise 8.5

--- part8/library/index.js
+++ part8/library/index.js
@@ -112,7 +112,7 @@ type Author {
 type Query {
 	bookCount: Int!
 	authorCount: Int!
-	allBooks: [Book!]!
+	allBooks(author: String): [Book!]!
 	allAuthors: [Author!]!
 }
 `
@@ -121,7 +121,12 @@ const resolvers = {
 	Query: {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
-		allBooks: () => books,
+		allBooks: (root, args) => {
+			if (!args.author) {
+				return books
+			}
+			return books.filter(b => b.author === args.author)
+		},
 		allAuthors: () => authors,
 	},
 	Author: {
