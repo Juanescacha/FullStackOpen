506f8f1f727630997fdcc74e3566506dae269882
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-02-27 13:25:48 -0500

    prettier

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,32 +1,32 @@
-const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+const Books = props => {
+	if (!props.show) {
+		return null
+	}
 
-  const books = []
+	const books = []
 
-  return (
-    <div>
-      <h2>books</h2>
+	return (
+		<div>
+			<h2>books</h2>
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>author</th>
+						<th>published</th>
+					</tr>
+					{books.map(a => (
+						<tr key={a.title}>
+							<td>{a.title}</td>
+							<td>{a.author}</td>
+							<td>{a.published}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 export default Books
