431ef5d5af811bbc51a54430f9ac8d6c58c964cc
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-03-01 16:08:06 -0500

    base code for exercises

--- part8/library/index.js
+++ part8/library/index.js
@@ -19,11 +19,11 @@ let authors = [
 		born: 1821,
 	},
 	{
-		name: "Joshua Kerievsky", // birthyear not known
+		name: "Joshua Kerievsky", // birth year unknown
 		id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
 	},
 	{
-		name: "Sandi Metz", // birthyear not known
+		name: "Sandi Metz", // birth year unknown
 		id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
 	},
 ]
