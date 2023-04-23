9fcdf7638d440a72b054b292cbd92312282e4f44
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-04-22 19:31:42 -0500

    Exercise 8.21 & 8.22

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -11,8 +11,8 @@ export const ALL_AUTHORS = gql`
 `
 
 export const ALL_BOOKS = gql`
-	query {
-		allBooks {
+	query AllBooks($genre: String) {
+		allBooks(genre: $genre) {
 			title
 			author {
 				name
