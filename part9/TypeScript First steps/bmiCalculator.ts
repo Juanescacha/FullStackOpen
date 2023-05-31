f90ea65d366aaf376a73602a8030a514e84d3733
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-05-31 00:09:21 -0500

    Exercise 9.6

--- part9/TypeScript First steps/bmiCalculator.ts
+++ part9/TypeScript First steps/bmiCalculator.ts
@@ -1,22 +1,22 @@
 export const calculateBmi = (height: number, weight: number): string => {
-	//	BMI Calculator
-	//	weight -> kg
-	//	height -> cm
-	let BMI = weight / (height / 100) ** 2
-	// let phrase = "Underweight (unhealthy weight)" ? BMI < 18.5 : "Normal (healthy weight)" ? BMI >= 18.5 && BMI < 25 : "Overweight (unhealthy weight)" ? BMI >= 25 && BMI < 30 : "Obese (unhealthy weight)"
-	let phrase =
-		BMI < 18.5
-			? "Underweight (unhealthy weight)"
-			: BMI >= 18.5 && BMI < 22.9
-			? "Normal (healthy weight)"
-			: BMI >= 22.9 && BMI < 24.9
-			? "Overweight (at risk)"
-			: "Overweight (moderately obese)"
+  //	BMI Calculator
+  //	weight -> kg
+  //	height -> cm
+  const BMI = weight / (height / 100) ** 2;
+  // let phrase = "Underweight (unhealthy weight)" ? BMI < 18.5 : "Normal (healthy weight)" ? BMI >= 18.5 && BMI < 25 : "Overweight (unhealthy weight)" ? BMI >= 25 && BMI < 30 : "Obese (unhealthy weight)"
+  const phrase =
+    BMI < 18.5
+      ? "Underweight (unhealthy weight)"
+      : BMI >= 18.5 && BMI < 22.9
+      ? "Normal (healthy weight)"
+      : BMI >= 22.9 && BMI < 24.9
+      ? "Overweight (at risk)"
+      : "Overweight (moderately obese)";
 
-	return phrase
-}
+  return phrase;
+};
 
-const h: number = Number(process.argv[2])
-const w: number = Number(process.argv[3])
+const h = Number(process.argv[2]);
+const w = Number(process.argv[3]);
 
-console.log(calculateBmi(h, w))
+console.log(calculateBmi(h, w));
--- part9/TypeScript First steps/index.ts
+++ part9/TypeScript First steps/index.ts
@@ -1,25 +1,25 @@
-import express from "express"
-import { calculateBmi } from "./bmiCalculator"
+import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 // import { calculateExercises } from "./exerciseCalculator"
 
-const app = express()
+const app = express();
 
 app.get("/hello", (_req, res) => {
-	res.send("Hello Full Stack!")
-})
+  res.send("Hello Full Stack!");
+});
 
 app.get("/bmi", (req, res) => {
-	const height = Number(req.query.height)
-	const weight = Number(req.query.weight)
-	const bmi = calculateBmi(height, weight)
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  const bmi = calculateBmi(height, weight);
 
-	res.send({
-		weight,
-		height,
-		bmi,
-	})
-})
+  res.send({
+    weight,
+    height,
+    bmi,
+  });
+});
 
 app.listen(3000, () => {
-	console.log("Server is listening on port 3000")
-})
+  console.log("Server is listening on port 3000");
+});
