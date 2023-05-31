f02bb91dda5a8aff51001ce8750a67a941d6bea5
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-05-31 16:23:43 -0500

    Exercise 9.8

---
+++ part9/patientor/backend/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+const app = express()
+app.use(express.json())
+
+const PORT = 3000
+
+app.get("/api/ping", (_req, res) => {
+	console.log("someone pinged here")
+	res.send("pong")
+})
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+	console.log(`http://localhost:${PORT}/ping`)
+})
