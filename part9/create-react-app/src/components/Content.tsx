a8af01e99a88d6afbd7bcb234b84d3ff7cda1747
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-07-10 22:16:51 -0500

    Exercise 9.15

--- part9/create-react-app/src/components/Content.tsx
+++ part9/create-react-app/src/components/Content.tsx
@@ -1,17 +1,70 @@
 import { CoursePart } from "../types"
 
+const assertNever = (value: never): never => {
+	throw new Error(
+		`Unhandled discriminated union member: ${JSON.stringify(value)}`
+	)
+}
+
+const Part = ({ part }: { part: CoursePart }) => {
+	switch (part.kind) {
+		case "basic":
+			return (
+				<p>
+					<b>
+						{part.name} {part.exerciseCount}
+					</b>
+					<br />
+					<i>{part.description}</i>
+				</p>
+			)
+		case "group":
+			return (
+				<p>
+					<b>
+						{part.name} {part.exerciseCount}
+					</b>
+					<br />
+					project exercises {part.groupProjectCount}
+				</p>
+			)
+		case "background":
+			return (
+				<p>
+					<b>
+						{part.name} {part.exerciseCount}
+					</b>
+					<br />
+					{part.description}
+					<br />
+					{part.backgroundMaterial}
+				</p>
+			)
+		case "special":
+			return (
+				<p>
+					<b>
+						{part.name} {part.exerciseCount}
+					</b>
+					<br />
+					{part.description} <br />
+					{part.requirements && part.requirements.join(", ")}
+				</p>
+			)
+		default:
+			return assertNever(part)
+	}
+}
+
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
 	return (
 		<>
-			<p>
-				{courseParts[0].name} {courseParts[0].exerciseCount}
-			</p>
-			<p>
-				{courseParts[1].name} {courseParts[1].exerciseCount}
-			</p>
-			<p>
-				{courseParts[2].name} {courseParts[2].exerciseCount}
-			</p>
+			{courseParts.map((part, index) => (
+				<Part
+					part={part}
+					key={index}
+				/>
+			))}
 		</>
 	)
 }
--- part9/create-react-app/src/types.ts
+++ part9/create-react-app/src/types.ts
@@ -1,4 +1,33 @@
-export interface CoursePart {
+interface CoursePartBase {
 	name: string
 	exerciseCount: number
 }
+
+interface CoursePartDescription extends CoursePartBase {
+	description: string
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+	kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+	groupProjectCount: number
+	kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+	backgroundMaterial: string
+	kind: "background"
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+	requirements: string[]
+	kind: "special"
+}
+
+export type CoursePart =
+	| CoursePartBasic
+	| CoursePartGroup
+	| CoursePartBackground
+	| CoursePartSpecial
